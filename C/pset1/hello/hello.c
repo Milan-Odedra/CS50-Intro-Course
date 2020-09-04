3f49bcafea702323f1b599db74275afa572a78f6
Milan Odedra <Milan26@live.co.uk>
2020-09-04 21:15:45 +0100

    Update hello.c

--- C/pset1/hello/hello.c
+++ C/pset1/hello/hello.c
@@ -1,1 +1,8 @@
+#include <stdio.h>
+#include <cs50.h>
 
+int main(void)
+{
+    string name = get_string("What is your name?\n"); /* Variable called name asks the user to input their name and saves it */
+    printf("hello, %s\n", name);                      /* prints hello with the stored varibale */
+}
