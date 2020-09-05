5569c4d3cd7c77452620a63dbf4e1add0db6060f
Milan Odedra <Milan26@live.co.uk>
2020-09-05 14:26:12 +0100

    Update mario.c
    Solution for mario less in cs50.
    Replicating the end level in super mario bros with the ascending blocks using hashes.

--- C/pset1/mario/less/mario.c
+++ C/pset1/mario/less/mario.c
@@ -1,1 +1,27 @@
+# include <stdio.h>
+# include <cs50.h>
 
+int main (void)
+{
+    int height, row, column, space;   /* int datatype to store variables */
+    do
+    {
+
+    height = get_int ("Enter a height: ");  /* Prompts the user to enter a height */ 
+    }
+    while (height < 1 || height > 8);       /* while loop to keep prompting user to enter a vaild height */
+
+    for (row = 0; row < height; row++)      /* sets row to 0 and when row is less than height prints 1 row */
+    {
+        for (space = 0; space < height - row - 1; space++)    /* add spaces to right allign the column */
+        {
+            printf(" ");                                      /* prints spaces with the formula */
+        }
+
+        for (column = 0; column <= row; column++)             /* when column is <= row print # */
+        {
+            printf("#");                         
+        }
+        printf("\n");            /* prints new line */
+    }
+}
