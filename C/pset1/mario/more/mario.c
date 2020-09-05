b245ff002127179a3a9cb2a640ae8dfc4b33e57d
Milan Odedra <Milan26@live.co.uk>
2020-09-05 16:12:19 +0100

    Update mario.c

--- C/pset1/mario/more/mario.c
+++ C/pset1/mario/more/mario.c
@@ -1,1 +1,32 @@
+# include <stdio.h>
+# include <cs50.h>
 
+int main (void)
+{
+    int height, line, column, space;
+    do
+    {
+
+    height = get_int ("Please enter a height: ");
+    }
+    while (height < 1 || height > 8);
+
+    for (line = 0; line < height; line++)
+    {
+        for (space = 0; space < height - line - 1; space++)
+        {
+            printf(" ");
+        }
+
+        for (column = 0; column <= line; column++)
+        {
+            printf("#");
+        }
+        printf("  ");
+        for (column = 0; column <= line; column++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
